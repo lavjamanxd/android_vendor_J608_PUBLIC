8e524fd5d7fd0c1b5775051f44f2cf3ad1b6a131
lavjamanxd <lavjamanxd@gmail.com>
2015-08-13 07:11:29 -0700

    Updated proprietary prebuild stuff

---
+++ J608_PUBLIC-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/J608_PUBLIC/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/J608_PUBLIC/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/J608_PUBLIC/etc/firmware,system/etc/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/J608_PUBLIC/etc/wifi,system/etc/wifi)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/doogee/J608_PUBLIC/gapps/system,system)
+
